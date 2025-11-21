de381b3602479badecb85284fb9d85724845a913
d9394 <15172911+d9394@users.noreply.github.com>
2025-11-21 16:10:36 +0800

    Update init.sh for cron logging and exit behavior

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Starting"
-echo -e "test" | sudo -S crond -f -c /var/spool/cron/crontabs -L /Logs/cron.log
-tail -f /Logs/cron.log
+echo -e "test" | sudo -S crond -f -c /var/spool/cron/crontabs -l 5 -L /dev/stdout | awk '{print "[" strftime("%Y-%m-%d %H:%M:%S") "]", $0}' > /Logs/cron.log
+tail -f /dev/null
 echo 'Exiting'
